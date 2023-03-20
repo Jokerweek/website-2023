e7af0eaa4f5db3ce1c97cebac8b2194da9d4fa75
philippe <philippe.soubrier@gmail.com>
2023-03-20 09:55:50 +0100

    weekmenu

--- src/pages/weekmenu.tsx
+++ src/pages/weekmenu.tsx
@@ -7,47 +7,63 @@ import { SubPage } from "~/components";
 export default function Weekmenu() {
   return (
     <SubPage title="Weekmenu" image="/images/test_1.png">
-      <h2 className="font-subTitle tracking-normal mt-5 text-2xl text-primary ">MAANDAG</h2>      
-      Yeah, listen up, I&apos;m &apos;bout to drop some heat Let me tell you
-      &apos;bout this wrap that can&apos;t be beat It&apos;s packed with
-      veggies, meat or tofu A perfect lunch that&apos;s easy to go to Wrap it up
-      tight and you&apos;re good to go, that&apos;s the truth It&apos;s healthy,
-      delicious, and quite the proof!
-      <br/><br/>
-      Kleuren zo helder en smaken zoet, Voedzaam en gezond, een feest voor wie
-      het proeft, Sappig en lekker, zo&apos;n verfrissend geluid, Wat ben ik?
-      Een heerlijk stuk fruit!
+      <h2 className="mt-5 font-subTitle text-2xl tracking-normal text-primary ">
+        MAANDAG
+      </h2>
+      Yeah, listen up, I&apos;m &apos;bout to drop some heat
+      <br /> Let me tell you &apos;bout this wrap that can&apos;t be beat
+      <br /> It&apos;s packed with veggies, meat or tofu
+      <br /> A perfect lunch that&apos;s easy to go to
+      <br /> Wrap it up tight and you&apos;re good to go, that&apos;s the truth
+      <br /> It&apos;s healthy, delicious, and quite the proof!
+      <br /> <br />
+      Kleuren zo helder en smaken zoet,
+      <br /> Voedzaam en gezond, een feest voor wie het proeft,
+      <br /> Sappig en lekker, zo&apos;n verfrissend geluid,
+      <br /> Wat ben ik? Een heerlijk stuk fruit!
       <hr className="my-2" />
-      <h2 className="font-subTitle tracking-normal mt-5 text-2xl text-primary ">DINSDAG</h2>
-      Ik ben een salade die geen sla bevat, Maar in plaats daarvan heb ik pasta
-      als schat. Een mix van texturen en smaken, dat is mijn kracht, Een
-      perfecte aanvulling voor elke zomerse maaltijd in alle pracht.
-      <br/><br/>
-     
-      Een lekkernij, zoet en zacht Een traktatie die je blij maakt, dag en nacht
-      Een Belgisch gebak, een ware traktatie Kom snel je kent de locatie !!
-      
+      <h2 className="mt-5 font-subTitle text-2xl tracking-normal text-primary ">
+        DINSDAG
+      </h2>
+      Ik ben een salade die geen sla bevat,
+      <br /> Maar in plaats daarvan heb ik pasta als schat.
+      <br /> Een mix van texturen en smaken, dat is mijn kracht,
+      <br /> Een perfecte aanvulling voor elke zomerse maaltijd in alle pracht.
+      <br /> <br /> Een lekkernij, zoet en zacht
+      <br /> Een traktatie die je blij maakt, dag en nacht
+      <br /> Een Belgisch gebak, een ware traktatie
+      <br /> Kom snel je kent de locatie !!
       <hr className="my-2" />
-      <h2 className="font-subTitle tracking-normal mt-5 text-2xl text-primary ">WOENSDAG</h2>
-     
-      Wist je dat … mijn naam afkomstig is van een Grieks gegist gerstendrankje?
-      Wist je dat … ik in het Engels vernoemd ben naar een Britse graaf die een
-      grote fan was van kapitein James Cook? Wist je dat …
+      <h2 className="mt-5 font-subTitle text-2xl tracking-normal text-primary ">
+        WOENSDAG
+      </h2>
+      <br /> Wist je dat … mijn naam afkomstig is van een Grieks gegist
+      gerstendrankje?
+      <br /> Wist je dat … ik in het Engels vernoemd ben naar een Britse graaf
+      die een grote fan was van kapitein James Cook?
+      <br /> Wist je dat …
       <hr className="my-2" />
-      <h2 className="font-subTitle tracking-normal mt-5 text-2xl text-primary ">DONDERDAG</h2>
-      <p className="mb-4">
-        Vandaag eten we een maaltijd, maar niet op het juiste moment. We eten
-        het &apos;s middags, al is het meer voor &apos;s ochtends gekend. Het is
-        een feest voor degenen die hongerig zijn, De keuze tussen al die
-        gerechtjes is voor iedereen fijn. Geen breakfast, geen brunch, maar wel
-        een breakfast for lunch!
-      </p>
+      <h2 className="mt-5 font-subTitle text-2xl tracking-normal text-primary ">
+        DONDERDAG
+      </h2>
+      <br /> Vandaag eten we een maaltijd, maar niet op het juiste moment.
+      <br /> We eten het &apos;s middags, al is het meer voor &apos;s ochtends
+      gekend.
+      <br /> Het is een feest voor degenen die hongerig zijn,
+      <br /> De keuze tussen al die gerechtjes is voor iedereen fijn.
+      <br /> Geen breakfast, geen brunch, maar wel een breakfast for lunch!
       <Accordion
-        style={{ boxShadow: "none", backgroundColor: "transparent" }}
+        sx={{
+          marginTop: "rem",
+          boxShadow: "none",
+          backgroundColor: "transparent",
+        }}
         className="border border-dashed"
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <h3 className="font-subTitle tracking-normal">Info over quinoa WWF:</h3>
+          <h3 className="font-subTitle tracking-normal">
+            Info over quinoa WWF:
+          </h3>
         </AccordionSummary>
         <AccordionDetails>
           <ul className="ml-2 list-square">
@@ -73,12 +89,23 @@ export default function Weekmenu() {
           </ul>
         </AccordionDetails>
       </Accordion>
-      <br/>
-      Een snack zo beroemd en fijn, Geboren in België, zo heerlijk en zo klein.
-      Goudbruin, knapperig, en dun.
+      <br />
+      Een Belgischere maaltijd dan dit is onmogelijk.
+      <br /> No further information needed.
       <hr className="my-2" />
-      <h2 className="font-subTitle tracking-normal mt-5 text-2xl text-primary ">VRIJDAG</h2>
-      
+      <h2 className="mt-5 font-subTitle text-2xl tracking-normal text-primary ">
+        VRIJDAG
+      </h2>
+      O, aardse schat met witte schil,
+      <br /> Gegroeid in de grond, zo zacht en stil.
+      <br /> Jouw hartig en zoet aroma, dat verraadt,
+      <br /> Een rijke smaak die ons verrukt en opbeurt, op elke maat.
+      <br /> <br />
+      O, knol en wortel, gezegend door de natuur,
+      <br /> Jouw zachte smaak maakt ons hart zo puur.
+      <br /> En dus zullen we blijven proeven en genieten,
+      <br /> Van jouw aardse goedheid, die ons nooit zal verlaten of
+      verschieten.
     </SubPage>
   );
 }
