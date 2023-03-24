56a21be2fb075e2ab08e4db4ea202e54111b8306
philippe <philippe.soubrier@gmail.com>
2023-03-24 23:06:39 +0100

    opdracht

--- src/pages/expo.tsx
+++ src/pages/expo.tsx
@@ -2,9 +2,16 @@ import { SubPage } from "~/components";
 
 export default function Expo() {
   return (
-    <SubPage title="Expo" image="/images/test_1.png">
-      <h2 className="font-subTitle tracking-normal mt-5 text-2xl text-primary">TEST</h2>
-      Ut sint incididunt tempor Lorem cupidatat laborum elit eu qui elit ea id.
+    <SubPage title="Expo" image="/images/bg/doorklik/expo.png">
+      <h2 className="mt-5 font-subTitle text-2xl tracking-normal text-primary">
+        LINK NAAR{" "}
+        <a
+          href="https://www.google.com/maps/d/u/2/edit?mid=12NzD1bN9on-6eTJJrO7H_wWXAHSIwU8&usp=sharing"
+          className="underline underline-offset-4"
+        >
+          GOOGLE MAPS
+        </a>
+      </h2>
     </SubPage>
   );
 }
