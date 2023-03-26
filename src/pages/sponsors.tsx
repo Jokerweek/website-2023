19356cb2b56621e4d67362968c4dc70c45c27969
philippe <philippe.soubrier@gmail.com>
2023-03-26 23:26:30 +0200

    sponsors

---
+++ src/pages/sponsors.tsx
@@ -0,0 +1,9 @@
+import { SponsorsLogos, SubPage } from "~/components";
+
+export default function Sponsors() {
+  return (
+    <SubPage title="Sponsors" image="/images/test_1.png">
+      <SponsorsLogos />
+    </SubPage>
+  );
+}
